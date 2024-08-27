8715cb159ed4660156e02bc86405cad0b78735f4
J Cole Morrison <me@jcolemorrison.com>
2024-08-27 01:48:23 -0700

    add hcp tf org variables and arrange required ones

--- services/variables.tf
+++ services/variables.tf
@@ -18,15 +18,21 @@ variable "aws_default_region" {
   default     = "us-east-1"
 }
 
-variable "eks_cluster_name" {
-  type        = string
-  description = "The name of the EKS cluster."
-}
-
 variable "default_container_image" {
   type        = string
   description = "Default service container image"
   default     = "nicholasjackson/fake-service:v0.26.0"
+}
+
+variable "ui_service_name" {
+  type        = string
+  description = "The name of the UI service. Used for namespace and name."
+  default     = "ui"
+}
+
+variable "eks_cluster_name" {
+  type        = string
+  description = "The name of the EKS cluster."
 }
 
 variable "public_subnet_ids" {
@@ -34,8 +40,14 @@ variable "public_subnet_ids" {
   description = "The list of public subnet IDs."
 }
 
-variable "ui_service_name" {
+### Must be set in the workspace or via the CLI
+
+variable "hcp_terraform_organization_name" {
   type        = string
-  description = "The name of the UI service. Used for namespace and name."
-  default     = "ui"
+  description = "The name of the Terraform organization."
+}
+
+variable "hcp_terraform_infrastructure_workspace_name" {
+  type        = string
+  description = "The name of the infrastructure workspace."
 }
