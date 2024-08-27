bca4fa48a18794fca853388c0e63d3c92685089a
J Cole Morrison <me@jcolemorrison.com>
2024-08-26 23:42:55 -0700

    remove hcp vars and output

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -40,23 +40,3 @@ variable "eks_cluster_version" {
   description = "The version of Kubernetes for EKS to use."
   default     = "1.29"
 }
-
-# HCP Variables
-
-variable "hvn_cidr_block" {
-  type        = string
-  description = "Cidr block for the HCP HVN. Cannot overlap with VPC CIDR block."
-  default     = "172.25.16.0/20"
-}
-
-variable "hcp_boundary_worker_tags" {
-  type        = list(string)
-  description = "Tags to apply to the Boundary worker instance."
-  default     = ["worker"]
-}
-
-variable "hcp_boundary_worker_count" {
-  type        = number
-  description = "The number of Boundary worker instances to deploy."
-  default     = 2
-}
