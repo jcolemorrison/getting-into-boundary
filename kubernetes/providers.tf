4bf958a18fbbb085e7ea1bededd354e25389a304
J Cole Morrison <me@jcolemorrison.com>
2024-08-26 23:29:46 -0700

    base eks deployment sans hcp

--- kubernetes/providers.tf
+++ kubernetes/providers.tf
@@ -14,3 +14,26 @@ terraform {
     }
   }
 }
+
+# Alternatively you could pass these values via workspaces, variables, TFE provider, or TF State
+data "aws_eks_cluster" "main" {
+  name = local.eks_cluster_name
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = local.eks_cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
+}
