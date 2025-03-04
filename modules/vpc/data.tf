6feb7b9a3cb767f056fb693ca8a2420cdc7b32e4
J Cole Morrison <me@jcolemorrison.com>
2025-03-04 14:49:43 -0800

    change vpc filtering in module

--- modules/vpc/data.tf
+++ modules/vpc/data.tf
@@ -5,7 +5,7 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "available" {
   state = "available"
   filter {
-    name   = "group-name"
+    name   = "region-name"
     values = [data.aws_region.current.name]
   }
 }
