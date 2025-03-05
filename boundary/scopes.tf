9ed3b35599327083f90a6a88240623e5a2ea6924
J Cole Morrison <me@jcolemorrison.com>
2025-03-04 22:36:06 -0800

    added more sample projects

--- boundary/scopes.tf
+++ boundary/scopes.tf
@@ -14,3 +14,19 @@ resource "boundary_scope" "gib_project" {
   auto_create_default_role = true
   auto_create_admin_role   = true
 }
+
+resource "boundary_scope" "gib_project_foo" {
+  scope_id                 = boundary_scope.gib_org.id
+  name                     = "getting-into-boundary-foo"
+  description              = "Getting into Boundary project scope foo"
+  auto_create_default_role = true
+  auto_create_admin_role   = true
+}
+
+resource "boundary_scope" "gib_project_bar" {
+  scope_id                 = boundary_scope.gib_org.id
+  name                     = "getting-into-boundary"
+  description              = "Getting into Boundary project scope bar"
+  auto_create_default_role = true
+  auto_create_admin_role   = true
+}
