2459da98da0a87794450c9004a81f8c400e07b3d
J Cole Morrison <me@jcolemorrison.com>
2025-03-05 12:56:52 -0800

    add back controller led worker with script changes

--- boundary/workers.tf
+++ boundary/workers.tf
@@ -1,39 +1,39 @@
 # Worker that uses controller led authorization
-# resource "boundary_worker" "ctrl_led_worker" {
-#   scope_id    = "global"
-#   name        = "${var.project_name}-worker"
-#   description = "self managed worker with controller led auth"
-# }
+resource "boundary_worker" "ctrl_led_worker" {
+  scope_id    = "global"
+  name        = "${var.project_name}-worker"
+  description = "self managed worker with controller led auth"
+}
 
 data "aws_ssm_parameter" "al2023" {
   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
 }
 
-# resource "aws_instance" "boundary_worker_ctrl_led" {
-#   # count = var.boundary_worker_count
+resource "aws_instance" "boundary_worker_ctrl_led" {
+  # count = var.boundary_worker_count
 
-#   ami                         = var.boundary_ami != "" ? var.boundary_ami : data.aws_ssm_parameter.al2023.value
-#   associate_public_ip_address = true
-#   instance_type               = "t3.micro"
-#   iam_instance_profile        = aws_iam_instance_profile.boundary_worker_profile.name
-#   key_name                    = local.ec2_kepair_name
-#   vpc_security_group_ids      = [local.boundary_worker_security_group_id]
+  ami                         = var.boundary_ami != "" ? var.boundary_ami : data.aws_ssm_parameter.al2023.value
+  associate_public_ip_address = true
+  instance_type               = "t3.micro"
+  iam_instance_profile        = aws_iam_instance_profile.boundary_worker_profile.name
+  key_name                    = local.ec2_kepair_name
+  vpc_security_group_ids      = [local.boundary_worker_security_group_id]
 
-#   # constrain to number of public subnets
-#   subnet_id = local.public_subnet_ids[0]
+  # constrain to number of public subnets
+  subnet_id = local.public_subnet_ids[0]
 
-#   user_data = templatefile("${path.module}/scripts/boundary-worker-ctrl-led.sh", {
-#     CONTROLLER_ADDRESSES                  = jsonencode(local.boundary_controller_private_ips)
-#     CONTROLLER_GENERATED_ACTIVATION_TOKEN = boundary_worker.ctrl_led_worker.controller_generated_activation_token
-#     WORKER_ID                             = boundary_worker.ctrl_led_worker.id
-#   })
+  user_data = templatefile("${path.module}/scripts/boundary-worker-ctrl-led.sh", {
+    CONTROLLER_ADDRESSES                  = jsonencode(local.boundary_controller_private_ips)
+    CONTROLLER_GENERATED_ACTIVATION_TOKEN = boundary_worker.ctrl_led_worker.controller_generated_activation_token
+    WORKER_ID                             = boundary_worker.ctrl_led_worker.id
+  })
 
-#   user_data_replace_on_change = true
+  user_data_replace_on_change = true
 
-#   tags = {
-#     Name = "boundary-worker-ctrl-led"
-#   }
-# }
+  tags = {
+    Name = "boundary-worker-ctrl-led"
+  }
+}
 
 # Worker that uses KMS led authorization
 resource "aws_instance" "boundary_worker_kms_led" {
