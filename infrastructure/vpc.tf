ba7f1176835ebd7aea6d9adefba03365e8c577fc
J Cole Morrison <me@jcolemorrison.com>
2025-03-20 21:54:49 -0700

    add all correct routes to tgw

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -5,17 +5,18 @@ module "vpc" {
   name       = var.project_name
 }
 
-## Transit Gateway Attachments and Routing
-resource "aws_ec2_transit_gateway_vpc_attachment" "main" {
-  vpc_id             = module.vpc.id
-  subnet_ids         = module.vpc.private_subnet_ids
-  transit_gateway_id = aws_ec2_transit_gateway.main_tgw.id
-}
-
 resource "aws_route" "tgw_route_private" {
-  destination_cidr_block = var.vpc_cidr_block
+  destination_cidr_block = var.hvn_cidr_block
   route_table_id         = module.vpc.private_route_table_id
   transit_gateway_id     = aws_ec2_transit_gateway.main_tgw.id
 
-  depends_on = [ aws_ec2_transit_gateway_vpc_attachment.main ]
+  depends_on = [ aws_ec2_transit_gateway_vpc_attachment.main_tgw ]
+}
+
+resource "aws_route" "tgw_route_public" {
+  destination_cidr_block = var.hvn_cidr_block
+  route_table_id         = module.vpc.public_route_table_id
+  transit_gateway_id     = aws_ec2_transit_gateway.main_tgw.id
+
+  depends_on = [ aws_ec2_transit_gateway_vpc_attachment.main_tgw ]
 }
