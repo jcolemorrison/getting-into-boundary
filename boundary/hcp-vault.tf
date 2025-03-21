944cb2a3ad5764a2c0215fe4eed1553cc1904a2b
J Cole Morrison <me@jcolemorrison.com>
2025-03-20 21:17:20 -0700

    add kv secret for database creds

--- boundary/hcp-vault.tf
+++ boundary/hcp-vault.tf
@@ -4,3 +4,21 @@ resource "vault_mount" "static" {
   options     = { version = "2" }
   description = "For static secrets related to ${var.project_name}"
 }
+
+resource "vault_kv_secret_v2" "postgres" {
+  mount               = vault_mount.static.path
+  name                = local.database_name
+  delete_all_versions = true
+
+  data_json = <<EOT
+{
+  "username": "${local.database_username}",
+  "password": "${local.database_password}"
+}
+EOT
+}
+
+data "vault_kv_secret_v2" "postgres" {
+  mount = vault_kv_secret_v2.postgres.mount
+  name  = vault_kv_secret_v2.postgres.name
+}
